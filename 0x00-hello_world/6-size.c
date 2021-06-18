49209081fcd88491530c89c132bdf8a56b150cac
jrichard1909 <jrichard1909@gmail.com>
2021-06-17 22:05:10 -0700

    The C program 6-size.c that prints the size of various types on the computer it is compiled and run on, was created.

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", siseof(char);
+	printf("Size of an int: %d byte(s)\n", siseof(int);
+	printf("Size of a long int: %d byte(s)\n", siseof(long int);
+	printf("Size of a long long: %d byte(s)\n", siseof(long long int);
+	printf("Size of a float: %d byte(s)\n", siseof(float);
+	return (0);
+}
