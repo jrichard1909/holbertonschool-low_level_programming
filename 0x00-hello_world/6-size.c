cf60ff85ba5ae6f618c2898636fb478c96e53315
jrichard1909 <jrichard1909@gmail.com>
2021-06-17 22:07:26 -0700

    The C program 6-size.c  was updated

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %d byte(s)\n", siseof(char);
-	printf("Size of an int: %d byte(s)\n", siseof(int);
-	printf("Size of a long int: %d byte(s)\n", siseof(long int);
-	printf("Size of a long long: %d byte(s)\n", siseof(long long int);
-	printf("Size of a float: %d byte(s)\n", siseof(float);
+	printf("Size of a char: %d byte(s)\n", sizeof(char);
+	printf("Size of an int: %d byte(s)\n", sizeof(int);
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int);
+	printf("Size of a long long: %d byte(s)\n", sizeof(long long int);
+	printf("Size of a float: %d byte(s)\n", sizeof(float);
 	return (0);
 }
