6c826ab182d56c96d5d98f3541437d033560e77b
jrichard1909 <jrichard1909@gmail.com>
2021-06-18 13:32:02 -0700

    The program 2-print_alphabet.c was created. This program will print the alphabet in lowercase, followed by a new line.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+	putchar (a);
+	putchar ('\n');
+
+	return (0);
+}
