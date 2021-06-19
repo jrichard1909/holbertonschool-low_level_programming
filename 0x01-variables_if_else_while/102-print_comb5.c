a23ed27557f3561109e59e86e7e5dcd9a9ed57d5
jrichard1909 <jrichard1909@gmail.com>
2021-06-18 23:44:00 -0700

    The program 102-print_comb5.c was created. This program will print all possible combinations of two two-digit numbers.The numbers should range from 0 to 99. The two numbers should be separated by a space. All numbers should be printed with two digits. 1 should be printed as 01. The combination of numbers must be separated by comma, followed by a space

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		for (b = 0 ; b <= 9 ; b++)
+		{
+			for (c = 0 ; c <= 9 ; c++)
+			{
+				for (d = 0 ; d <= 9 ; d++)
+				{
+					if (a <= c && b < d)
+					{
+					putchar (a + '0');
+					putchar (b + '0');
+					putchar (32);
+					putchar (c + '0');
+					putchar (d + '0');
+					if (a <= 9 && b < 9)
+					{
+						putchar (44);
+						putchar (32);
+					}
+					}
+				}
+			}
+		}
+	}
+	putchar ('\n');
+
+	return (0);
+}
