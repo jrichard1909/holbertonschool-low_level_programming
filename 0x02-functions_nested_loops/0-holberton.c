979f6217cdb41d734a5e4d4264c370a05cc1f6d6
jrichard1909 <jrichard1909@gmail.com>
2021-06-22 10:44:51 -0700

    The program 0-holberton.c was updated.

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,4 +1,4 @@
-#include <unistd.h>
+#include "holberton.h"
 /**
  * main - Entry point
  *
@@ -6,6 +6,15 @@
  */
 int main(void)
 {
-write(1, "Holberton\n", 10);
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
+_putchar('\n');
 return (0);
 }
