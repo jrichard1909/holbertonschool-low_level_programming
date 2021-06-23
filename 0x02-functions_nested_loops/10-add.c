dd1ed4d219803747ef3942f269798e2a2b087ae6
jrichard1909 <jrichard1909@gmail.com>
2021-06-22 22:23:10 -0700

    The function 10-add.c was created. This function adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers and returns the result.
+ * @a: The number to print
+ * @b: the second number
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
