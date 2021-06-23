680306c2ec4cac772aa7e927babef8559238db0b
jrichard1909 <jrichard1909@gmail.com>
2021-06-22 19:00:57 -0700

    The program 7-print_last_digit.c was updated

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -12,12 +12,13 @@ int print_last_digit(int a)
 {
 	if (a < 0)
 	{
-		_putchar((a * -1) % 10 + '0');
-		return ((a * -1) % 10);
+		a = a * -1;
+		_putchar((a % 10) + '0');
+		return (a % 10);
 	}
 	else
 	{
-		_putchar(a % 10 + '0');
-		return ((a % 10));
+		_putchar((a % 10) + '0');
+		return (a % 10);
 	}
 }
