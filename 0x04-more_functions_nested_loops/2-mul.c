fb7b13ea799cd512c37046d4a37ba379afac6183
jrichard1909 <jrichard1909@gmail.com>
2021-06-24 13:08:33 -0700

    The function 2-mul.c was created. This function multiplies two integers.

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * mul - prints the last digit of a number
+ * @a: The number to print
+ * @b: The second number
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
