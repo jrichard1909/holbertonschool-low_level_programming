cc4add8c2dc1c907c42719747b431de49a009171
jrichard1909 <jrichard1909@gmail.com>
2021-06-24 15:17:58 -0700

    The function 8-print_square.c was created. This function prints a square, followed by a new line.

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,7 +2,7 @@
 
 /**
  * print_square - prints a square, followed by a new line.
- * @size The number to print
+ * @size: The number to print
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
@@ -11,7 +11,7 @@
 void print_square(int size)
 {
 	int a, b;
-	
+
 	if (size > 0)
 	{
 		for (a = 0; a < size; a++)
