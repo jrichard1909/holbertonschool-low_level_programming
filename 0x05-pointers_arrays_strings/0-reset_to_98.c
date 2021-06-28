cd238b0bf8bcbe52019c9dddb5f36045395a25e4
jrichard1909 <jrichard1909@gmail.com>
2021-06-28 12:38:55 -0700

    The function 101-natural.cc was created. This function takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - prints the last digit of a number
+ * @n: The number to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
