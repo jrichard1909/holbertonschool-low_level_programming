a61aa14ae41790f9706a986e59a4afa4e979d90b
jrichard1909 <jrichard1909@gmail.com>
2021-06-28 21:23:42 -0700

    The function 5-rev_string.c was created. This function reverses a string.

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * rev_string - reverses a string.
+ * @s: The number to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void rev_string(char *s)
+{
+	int len = 0, cont1, cont2 = 0;
+	char *str, a[10], char *st;
+
+	st = a;
+	for (str = s; *str != '\0'; str++)
+	{
+		st[len] = s[len];
+		len++;
+	}
+
+	for (cont1 = len - 1; cont1 >= 0; cont1--)
+	{
+		s[cont2] = st[cont1];
+		cont2++;
+	}
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -4,3 +4,4 @@ void swap_int(int *a, int *b);
 void _puts(char *str);
 int _putchar(char c);
 void print_rev(char *s);
+void rev_string(char *s);
