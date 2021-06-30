e74a6550e06860055c6bd2da0a1c5546028fc624
jrichard1909 <jrichard1909@gmail.com>
2021-06-30 16:12:47 -0700

    The function 100-atoi.c was created. This function convert a string to an integer.

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * _atoi  - prints the last digit of a number
+ * @s: The number to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _atoi(char *s)
+{
+	int num = 0, numsigno = 1;
+	char signo = '+';
+
+	while (*s < '0' || *s > '9')
+	{
+		if (*s == '-' || *s == '+')
+		{
+		signo = (signo == *s) ? '+' : '-';
+		}
+		s++;
+	}
+	numsigno = 1 - 2 * (signo == '-');
+
+	while (*s >= '0' && *s <= '9')
+	{
+		num = num * 10 + *s - '0';
+		s++;
+	}
+
+	return (num * numsigno);
+}
