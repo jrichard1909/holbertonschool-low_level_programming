cfe259e48afb20df047650da46307cab5f04d72d
jrichard1909 <jrichard1909@gmail.com>
2021-07-05 15:01:16 -0700

    The function 0-memset.c was created. This function fills memory with a constant byte.

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * _memset  - prints the last digit of a number
+ * @s: Pointer
+ * @b: byte
+ * @n : number of byte
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int count;
+
+	for (count = 0; count < n; count++)
+	{
+		s[count] = b;
+	}
+
+	return (0);
+}
