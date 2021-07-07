d0cf618c6754af4a2157b2e87e11f8a185e13aad
jrichard1909 <jrichard1909@gmail.com>
2021-07-06 22:06:22 -0700

    The function 100-set_string.c was created. This function sets the value of a pointer to a char.

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * set_string - prints the last digit of a number
+ * @s: The number to print
+ * @to: a
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
