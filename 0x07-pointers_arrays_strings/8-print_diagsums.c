43e2d602c8634e77a0fec3a742a28f253ac42806
jrichard1909 <jrichard1909@gmail.com>
2021-07-06 22:02:02 -0700

    The function 8-print_diagsums.c was created. This function prints the sum of the two diagonals of a square matrix of integers.

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * print_diagsums - prints the last digit of a number
+ * @a: The number to print
+ * @n: n
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void print_diagsums(int *a, int n)
+{
+	int i;
+
+	unsigned int sum, sum1;
+
+	sum = 0;
+	sum1 = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		sum += a[(n + 1) * i];
+		sum1 += a[(n - 1) * (i + 1)];
+	}
+
+	printf("%d, %d\n", sum, sum1);
+}
