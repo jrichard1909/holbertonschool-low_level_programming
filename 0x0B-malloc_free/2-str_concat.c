bf52e31f26ce9a2e0e0646e47bbdf5eef6220abb
jrichard1909 <jrichard1909@gmail.com>
2021-07-14 00:03:42 -0700

    function that concatenates two strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * str_concat - prints the last digit of a number
+ * @s1: s1
+ * @s2: s2
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int n1 = _strlen_recursion(s1), n2 = _strlen_recursion(s2), i1, i2;
+	char *s;
+
+	s = malloc(n1 + n2);
+	if (s == NULL)
+		return (0);
+	for (i1 = 0; i1 < n1; i1++)
+		s[i1] = s1[i1];
+	for (i2 = 0; i2 < n1 + n2; i2++)
+		s[i1 + i2] = s2[i2];
+
+	s[i1 + i2] = '\0';
+
+	return (s);
+}
+
+/**
+ * _strlen_recursion  - prints the last digit of a number
+ * @s: The number to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+
+	s++;
+	return (1 + _strlen_recursion(s));
+}
---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,4 @@
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+int _strlen_recursion(char *s);
+char *str_concat(char *s1, char *s2);
