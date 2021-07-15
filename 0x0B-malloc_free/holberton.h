c7a93934c215e94becc6f1c3e4e6f10aadd829e5
jrichard1909 <jrichard1909@gmail.com>
2021-07-15 16:12:28 -0700

    a

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -4,3 +4,7 @@ int _strlen_recursion(char *s);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+char **strtow(char *str);
+int wordslen(char *str);
+int lenstrw(char *str, int n);
+char *strw(char *str, int n, int len);
