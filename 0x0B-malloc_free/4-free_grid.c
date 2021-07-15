6086a4b732b070a3d464d0a5b7d5e358de2bbd08
jrichard1909 <jrichard1909@gmail.com>
2021-07-14 20:37:46 -0700

    function that frees a 2 dimensional grid previously created by your alloc_grid function.

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * free_grid- prints the last digit of a number
+ * @grid: The number to print
+ * @height: h
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+	free(grid);
+
+}
