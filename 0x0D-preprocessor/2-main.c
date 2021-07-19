7fed32bc5146c37483729156c475ade7768eacb8
jrichard1909 <jrichard1909@gmail.com>
2021-07-19 14:03:12 -0700

    program that prints the name of the file it was compiled from, followed by a new line.

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+#include <stdio.h>
+
+#define FILE __FILE__
+
+int main(void)
+{
+	printf("%s\n", FILE);
+
+	return (0);
+}
