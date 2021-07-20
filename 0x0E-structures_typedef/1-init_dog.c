77c7a46599524c6337d8006ac08c18f28533e78f
jrichard1909 <jrichard1909@gmail.com>
2021-07-19 20:09:06 -0700

    function that initialize a variable of type struct dog

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_do - prints the last digit of a number
+ * @d: The number to print
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: On success 1.
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
