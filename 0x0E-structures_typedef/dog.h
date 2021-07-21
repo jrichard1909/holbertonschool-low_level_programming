63e2b2a22e6199aa48402d60de1e491c6d13f736
jrichard1909 <jrichard1909@gmail.com>
2021-07-20 22:06:17 -0700

    5

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,5 +22,6 @@ typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 char *_strncpy(char *dest, char *src);
 int _strlen_recursion(char *s);
+void free_dog(dog_t *d);
 
 #endif
