77b3f1aeac057a44752400a32ff9878205359ebe
jrichard1909 <jrichard1909@gmail.com>
2021-07-22 13:49:07 -0700

    3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,34 @@
+#include <stddef.h>
+#include "3-calc.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @a: The number to print
+ *
+ * Return: On success 1.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
