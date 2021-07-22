e4a2c8f721fce5b5d3d14cfcfcd8262d80c858e1
jrichard1909 <jrichard1909@gmail.com>
2021-07-21 21:44:35 -0700

    1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @a: The number to print
+ *
+ * Return: On success 1.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
