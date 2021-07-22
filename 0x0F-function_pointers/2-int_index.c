aead527e0b2ac8f9de15458292ae2530b5cbb441
jrichard1909 <jrichard1909@gmail.com>
2021-07-21 21:58:31 -0700

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @a: The number to print
+ *
+ * Return: On success 1.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || cmp == NULL)
+		return(0);
+	if (size < 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+
+	return (-1);
+}
