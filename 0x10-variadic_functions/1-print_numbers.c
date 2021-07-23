1f8753ccf79ff9d1f2625b6288b8a993a3c64bf4
jrichard1909 <jrichard1909@gmail.com>
2021-07-22 19:00:43 -0700

    1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "variadic_functions.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @a: The number to print
+ *
+ * Return: On success 1.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i = 0;
+	va_list ap;
+		
+	if (separator == NULL || n == 0)
+		return;
+
+	va_start(ap, n);
+	for (i = 0; i < n - 1; i++)
+		printf("%d%s", va_arg(ap, int), separator);
+
+	printf("%d\n", va_arg(ap, int));
+	va_end(ap);
+}
