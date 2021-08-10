149c74c1a7d21664dc23eaed27872ec6f119d46e
jrichard1909 <jrichard1909@gmail.com>
2021-08-09 18:01:01 -0700

    main.h header file created, 0-read_textfile.c function created

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @a: The number to print
+ *
+ * Return: On success 1.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t read_n, write_n;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(letters);
+	if (buf == NULL)
+		return(0);
+
+	read_n = read(fd, buf, letters);
+	if (read_n == -1)
+	{
+		free(buf);
+		return (0);
+	}
+
+	write_n = write(2, buf, read_n);
+	if (write_n == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	close(fd);
+
+	return (write_n);
+}
