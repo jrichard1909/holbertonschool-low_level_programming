a1900c2c861f7686f79ffdcc9d9b5d545e1879a3
jrichard1909 <jrichard1909@gmail.com>
2021-08-09 22:20:50 -0700

    2-append_text_to_file.c function updated

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -30,7 +30,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		len++;
 
 	write_n = write(fd, text_content, len);
-	if (write_n == -1)
+	if (write_n == -1 || write_n != len)
 	{
 		close(fd);
 		return (-1);
