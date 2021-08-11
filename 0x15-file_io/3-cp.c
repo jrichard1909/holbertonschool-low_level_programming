b4f5612fd53fa990070ba53c33b4ffe4005c6114
jrichard1909 <jrichard1909@gmail.com>
2021-08-11 14:02:12 -0700

    3-cp.c updated

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,40 +1,35 @@
 #include "main.h"
 
 /**
- * creat_w__file - prints the last digit of a number
- * @filename: ac
- * @read_n: n
- * @buf: buf
+ * check_rwc - prints the last digit of a number
+ * @n : ssize_t
+ * @filename: name of file
+ * @ind_rwc: indicate 1 read, 2 write, 3 close
+ * @fd : fd
  *
  * Return: On success 1.
  */
 
-void creat_w__file(const char *filename, char buf[], ssize_t read_n)
+void check_rwc(ssize_t n, char *filename, int ind_rwc, ssize_t fd)
 {
 
-	int fd_to;
-	ssize_t write_n, close_n;
-
-	fd_to = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |
-	S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (fd_to == -1)
+	if (n == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
-
-	write_n = write(fd_to, buf, read_n);
-	if (write_n == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
-		exit(99);
-	}
-
-	close_n = close(fd_to);
-	if (close_n == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", (int)fd_to);
-		exit(100);
+		if (ind_rwc == 1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+			exit(98);
+		}
+		else if (ind_rwc == 2)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+			exit(99);
+		}
+		else if (ind_rwc == 3)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d", (int)fd);
+			exit(100);
+		}
 	}
 }
 
@@ -48,9 +43,9 @@ void creat_w__file(const char *filename, char buf[], ssize_t read_n)
 
 int main(int ac, char **av)
 {
-	ssize_t read_n = 1;
+	ssize_t read_n, write_n, close_n;
 	char buf[1024];
-	int fd_from;
+	int fd_from, fd_to;
 
 	if (ac != 3)
 	{
@@ -65,16 +60,25 @@ int main(int ac, char **av)
 		exit(98);
 	}
 
+	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR |
+	S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+
 	while ((read_n = read(fd_from, buf, 1024)) != 0)
 	{
-		if (read_n == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
-		}
-
-		creat_w__file(av[2], buf, read_n);
+		check_rwc(read_n, av[1], 1, fd_from);
+		write_n = write(fd_to, buf, read_n);
+		check_rwc(write_n, av[2], 2, fd_to);
 	}
+
+	close_n = close(fd_from);
+	check_rwc(close_n, NULL, 3, fd_from);
+	close_n = close(fd_to);
+	check_rwc(close_n, NULL, 3, fd_to);
 
 	return (0);
 }
