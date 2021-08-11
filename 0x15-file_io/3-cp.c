62ed6d9a555d1b1befd8cb155f004bb043ce24e3
jrichard1909 <jrichard1909@gmail.com>
2021-08-11 11:37:07 -0700

    3-cp.c updatedd

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,45 +1,15 @@
 #include "main.h"
 
 /**
- * read_tfile - prints the last digit of a number
+ * creat_w__file - prints the last digit of a number
  * @filename: ac
- * @letters: av
+ * @read_n: n
  * @buf: buf
  *
  * Return: On success 1.
  */
 
-
-ssize_t read_tfile(const char *filename, size_t letters, char *buf[])
-{
-	ssize_t read_n, close_n;
-	int fd_from;
-
-	fd_from = open(filename, O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		exit(98);
-	}
-
-	read_n = read(fd_from, buf, letters);
-	if (read_n == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
-		exit(98);
-	}
-
-	close_n = close(fd_from);
-	if (close_n == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", (int)fd_from);
-		exit(100);
-	}
-
-	return (read_n);
-}
-
-void creat_w__file(const char *filename, char *buf[], ssize_t read_n)
+void creat_w__file(const char *filename, char buf[], ssize_t read_n)
 {
 
 	int fd_to;
@@ -78,8 +48,9 @@ void creat_w__file(const char *filename, char *buf[], ssize_t read_n)
 
 int main(int ac, char **av)
 {
-	ssize_t read_n = 0;
-	char *buf[1024];
+	ssize_t read_n = 1;
+	char buf[1024];
+	int fd_from;
 
 	if (ac != 3)
 	{
@@ -87,9 +58,21 @@ int main(int ac, char **av)
 		exit(97);
 	}
 
-	while (read_n  == 0 || read_n == 1024)
+	fd_from = open(av[1], O_RDONLY);
+	if (fd_from == -1)
 	{
-		read_n = read_tfile(av[1], 1024, buf);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+
+	while ((read_n = read(fd_from, buf, 1024)) != 0)
+	{
+		if (read_n == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+			exit(98);
+		}
+
 		creat_w__file(av[2], buf, read_n);
 	}
 
