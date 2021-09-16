cf18d5c17b3d1a644ae075a1175a124d99121ed8
jrichard1909 <jrichard1909@gmail.com>
2021-09-16 08:14:58 -0700

    1-dlistint_len.c file created. Function that returns the number of elements in a linked dlistint_t list.

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked list
+ * @h: linked lsit
+ *
+ * Return: count.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
