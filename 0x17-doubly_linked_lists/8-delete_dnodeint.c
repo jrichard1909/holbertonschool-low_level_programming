493f416716981acaf9abde60637f3253ad8e7a80
jrichard1909 <jrichard1909@gmail.com>
2021-09-17 08:39:52 -0700

    8-delete_dnodeint.c file created

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - prints all the elements of a dlistint_t list
+ * @head: linked lsit
+ * @index: data
+ *
+ * Return: count.
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *new_head = *head;
+	unsigned int count = 0;
+	
+	while (new_head)
+	{
+		if (index == 0)
+		{
+			if (new_head->next)
+				new_head->next->prev = NULL;
+			*head = new_head->next;
+			free(new_head);
+			return (1);
+		}
+
+		if (count == index)
+			break;
+		count++;
+		new_head = new_head->next;
+	}
+	if (count != index || (count == 0 && !new_head))
+		return (-1);
+	new_head->prev->next = new_head->next;
+	new_head->next->prev = new_head->prev;
+	free(new_head);
+
+	return (1);
+}
