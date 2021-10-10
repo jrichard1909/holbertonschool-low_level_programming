47a591d80cca0977075ef84323314b35998ad189
jrichard1909 <jrichard1909@gmail.com>
2021-10-10 16:29:45 -0700

    3-hash_table_set.c file created

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
+ *
+ * Return: hash value
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node;
+	unsigned long int index;
+
+	if (strlen(key) == 0 || key == NULL || ht == NULL)
+		return (0);
+
+	index = hash_djb2((const unsigned char *)key) % ht->size;
+
+	if (ht->array[index]->key == key)
+	{
+		ht->array[index]->value = strdup(value);
+		return (1);
+	}
+	
+	new_node = malloc(sizeof(hash_node_t *));
+	
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+
+	if (ht->array[index] == NULL)
+		new_node->next = NULL;
+	else
+		new_node->next = ht->array[index];
+
+	ht->array[index] = new_node;
+	
+	return (1);
+}
