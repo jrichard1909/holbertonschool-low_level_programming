7346016cc4254930a8c31e246fc713515638abe1
jrichard1909 <jrichard1909@gmail.com>
2021-10-10 07:38:48 -0700

    0-hash_table_create.c fixed

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_ht = NULL;
-	unsigned long int i;
 
 	new_ht = malloc(sizeof(hash_table_t));
 	if (new_ht == NULL)
@@ -24,7 +23,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	new_ht->size = size;
-	new_ht->array = NULL
+	new_ht->array = NULL;
 
 	return (new_ht);
 }
