a77e161164d677bd8b744a2d6e47895f1ec8b0a6
jrichard1909 <jrichard1909@gmail.com>
2021-10-10 16:32:35 -0700

    files 1  y 2 created

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - implementation of the djb2 algorithm
+ * @key: key
+ * @size: string used to generate hash value
+ *
+ * Return: key index
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = hash_djb2(key) % size;
+
+	return (index);
+}
