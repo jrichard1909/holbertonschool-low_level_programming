3500dc98c4e94c9f485dbe880eadfcad5dedb9cb
jrichard1909 <jrichard1909@gmail.com>
2021-10-11 18:13:43 -0700

    5-hash_table_print.c fixed for betty

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,9 +1,8 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_get - function that retrieves a value associated with a key
+ * hash_table_print - function that retrieves a value associated with a key
  * @ht: hast table
- * @key: key
  * Return: the value associated with the element.
  */
 
@@ -12,14 +11,15 @@ void hash_table_print(const hash_table_t *ht)
 	unsigned long int i;
 	int ind = 0;
 	hash_node_t *tmp_node;
+
 	if (ht == NULL)
 		return;
 
 	printf("{");
-	for(i = 0; i < ht->size; i++)
+	for (i = 0; i < ht->size; i++)
 	{
 		tmp_node = ht->array[i];
-		while(tmp_node != NULL)
+		while (tmp_node != NULL)
 		{
 			if (ind != 0)
 				printf(", ");
