900c1d6403932c1b3709e8a69048ba378338bed8
jrichard1909 <jrichard1909@gmail.com>
2022-08-31 23:36:20 -0500

    update header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,7 +7,7 @@
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int func_binary(int *array, int value, size_t lo, size_t hi)
-void print_array(int *array, size_t lo, size_t hi)
+int func_binary(int *array, int value, size_t lo, size_t hi);
+void print_array(int *array, size_t lo, size_t hi);
 
 #endif
